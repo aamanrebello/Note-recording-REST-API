7b9d33c0b0a6eb4ec99dfc9b0234dfa2be60e89b
aamanrebello <aamanrebello@gmail.com>
2021-06-17 01:02:49 +0100

    Improve user-operated client

--- Client/user-operated-client.py
+++ Client/user-operated-client.py
@@ -14,7 +14,7 @@ def list_notes():
     print(r.text)
     print()
     # No need to ask user to provide ID after calling this function.
-    return user_id
+    return user_id, r.status_code != 404
 
 #------------------------------------------------------------------------------------------------------
 
@@ -55,20 +55,21 @@ while True:
 
     elif code == "3":
         # List notes first.
-        user_id = list_notes()
-        # Preparing url to send post request
-        post_url = "http://127.0.0.1:5003/" + user_id
-        # Obtaining user provided information.
-        topic = input("Please provide topic of the new personal note (must be unique): ")
-        content = input("Now you can enter the text content of your note: ")
-        entry = {
-            "topic" : topic,
-            "content" : content
-        }
-        # Post note
-        r = requests.post(post_url, json = entry)
-        print(r.text)
-        print()
+        user_id, id_exists = list_notes()
+        if id_exists:
+            # Preparing url to send post request
+            post_url = "http://127.0.0.1:5003/" + user_id
+            # Obtaining user provided information.
+            topic = input("Please provide topic of the new personal note (must be unique): ")
+            content = input("Now you can enter the text content of your note: ")
+            entry = {
+                "topic" : topic,
+                "content" : content
+            }
+            # Post note
+            r = requests.post(post_url, json = entry)
+            print(r.text)
+            print()
 
     #-----------------------------------------------------------------------------------------
 
@@ -77,93 +78,97 @@ while True:
         print("1: Prepend existing note,\t 2: Append existing note,\t 3: Rewrite existing note.\n")
         sub_code = input("ENTER THE OPERATION YOU WANT TO PERFORM. Any other code will skip this operation. ")
         # List notes first.
-        user_id = list_notes()
-        # # Preparing url and information to send put request based on sub_code
-        if sub_code == "1":
-            # Take in user information.
-            topic = input("Please provide topic of the existing personal note: ")
-            content = input("Now you can enter the prepend content of your note: ")
-            entry = {
-                "topic" : topic,
-                "content" : content
-            }
-            put_url = "http://127.0.0.1:5006/" + user_id + "/prepend"
+        user_id, id_exists = list_notes()
+        if id_exists:
+            # # Preparing url and information to send put request based on sub_code
+            if sub_code == "1":
+                # Take in user information.
+                topic = input("Please provide topic of the existing personal note: ")
+                content = input("Now you can enter the prepend content of your note: ")
+                entry = {
+                    "topic" : topic,
+                    "content" : content
+                }
+                put_url = "http://127.0.0.1:5006/" + user_id + "/prepend"
 
-        elif sub_code == "2":
-            # Take in user information.
-            topic = input("Please provide topic of the existing personal note: ")
-            content = input("Now you can enter the append content of your note: ")
-            entry = {
-                "topic" : topic,
-                "content" : content
-            }
-            put_url = "http://127.0.0.1:5006/" + user_id + "/append"
+            elif sub_code == "2":
+                # Take in user information.
+                topic = input("Please provide topic of the existing personal note: ")
+                content = input("Now you can enter the append content of your note: ")
+                entry = {
+                    "topic" : topic,
+                    "content" : content
+                }
+                put_url = "http://127.0.0.1:5006/" + user_id + "/append"
 
-        elif sub_code == "3":
-            # Take in user information.
-            topic = input("Please provide topic of the existing personal note: ")
-            content = input("Now you can enter the new content of your note: ")
-            entry = {
-                "topic" : topic,
-                "content" : content
-            }
-            put_url = "http://127.0.0.1:5006/" + user_id + "/rewrite"
-        else:
-            continue
-        s = requests.put(put_url, json = entry)
-        print(s.text)
-        print()
+            elif sub_code == "3":
+                # Take in user information.
+                topic = input("Please provide topic of the existing personal note: ")
+                content = input("Now you can enter the new content of your note: ")
+                entry = {
+                    "topic" : topic,
+                    "content" : content
+                }
+                put_url = "http://127.0.0.1:5006/" + user_id + "/rewrite"
+            else:
+                continue
+            s = requests.put(put_url, json = entry)
+            print(s.text)
+            print()
 
     #--------------------------------------------------------------------------------------------
 
     elif code == "5":
         # List notes first.
-        user_id = list_notes()
-        topic = input("Please provide topic of the personal note to be archived: ")
-        # Prepare URL to send put request
-        put_url = "http://127.0.0.1:5007/send/" + user_id + "/" + topic
-        # Send post request.
-        r = requests.put(put_url, data = {})
-        print(r.text)
-        print()
+        user_id, id_exists = list_notes()
+        if id_exists:
+            topic = input("Please provide topic of the personal note to be archived: ")
+            # Prepare URL to send put request
+            put_url = "http://127.0.0.1:5007/send/" + user_id + "/" + topic
+            # Send post request.
+            r = requests.put(put_url, data = {})
+            print(r.text)
+            print()
 
     #----------------------------------------------------------------------------------------------
 
     elif code == "6":
         # List notes first.
-        user_id = list_notes()
-        topic = input("Please provide topic of the archived note to be recalled: ")
-        # Prepare URL to send put request
-        put_url = "http://127.0.0.1:5007/recall/" + user_id + "/" + topic
-        # Send put request.
-        r = requests.put(put_url, data = {})
-        print(r.text)
-        print()
+        user_id, id_exists = list_notes()
+        if id_exists:
+            topic = input("Please provide topic of the archived note to be recalled: ")
+            # Prepare URL to send put request
+            put_url = "http://127.0.0.1:5007/recall/" + user_id + "/" + topic
+            # Send put request.
+            r = requests.put(put_url, data = {})
+            print(r.text)
+            print()
 
     #-------------------------------------------------------------------------------------
 
     elif code == "7":
         # List notes first.
-        user_id = list_notes()
-        print("1: Delete note, 2: Delete archive.\n")
-        sub_code = input("ENTER THE NUMBER OF THE OPERATION YOU WANT (Any other code will skip this operation). ")
-        # Prepare URL differently based on sub code.
-        if sub_code == "1":
-            topic = input("Please provide topic of the note to be deleted: ")
-            # Prepare URL for delete request.
-            del_url = "http://127.0.0.1:5005/note/" + user_id + "/" + topic
+        user_id, id_exists = list_notes()
+        if id_exists:
+            print("1: Delete note, 2: Delete archive.\n")
+            sub_code = input("ENTER THE NUMBER OF THE OPERATION YOU WANT (Any other code will skip this operation). ")
+            # Prepare URL differently based on sub code.
+            if sub_code == "1":
+                topic = input("Please provide topic of the note to be deleted: ")
+                # Prepare URL for delete request.
+                del_url = "http://127.0.0.1:5005/note/" + user_id + "/" + topic
 
-        elif sub_code == "2":
-            topic = input("Please provide topic of the archive to be deleted: ")
-            # Prepare URL for delete request.
-            del_url = "http://127.0.0.1:5005/archive/" + user_id + "/" + topic
+            elif sub_code == "2":
+                topic = input("Please provide topic of the archive to be deleted: ")
+                # Prepare URL for delete request.
+                del_url = "http://127.0.0.1:5005/archive/" + user_id + "/" + topic
 
-        else:
-            continue
-        # send delete request.
-        t = requests.delete(del_url)
-        print(t.text)
-        print()
+            else:
+                continue
+            # send delete request.
+            t = requests.delete(del_url)
+            print(t.text)
+            print()
 
     #---------------------------------------------------------------------------------------------------
 
